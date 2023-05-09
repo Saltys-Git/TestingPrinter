8912a697df70fe9ab6a6517714102868c7742eda
Saltys-Git <sidurrahmansiam@gmail.com>
2023-05-09 21:50:08 +0600

    first commit

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,5 +1,8 @@
 import './globals.css'
 import { Inter } from 'next/font/google'
+import {QueryClient} from "@tanstack/query-core";
+import {QueryClientProvider} from "@tanstack/react-query";
+import {queryClient} from "@/app/queryClient";
 
 const inter = Inter({ subsets: ['latin'] })
 
@@ -15,7 +18,11 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
-      <body className={inter.className}>{children}</body>
+      <body className={inter.className}>
+      <QueryClientProvider client={queryClient}>
+        {children}
+      </QueryClientProvider>
+      </body>
     </html>
   )
 }
