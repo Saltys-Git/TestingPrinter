b39fc2656b2771b497f53fb2477b4f8c120067a7
Saltys-Git <sidurrahmansiam@gmail.com>
2023-05-10 23:34:19 +0600

    Forth commit

--- src/app/HelloWorldPrinter.tsx
+++ src/app/HelloWorldPrinter.tsx
@@ -15,27 +15,27 @@ export default function HelloWorldPrinter() {
                         <div className="flex flex-wrap -m-2">
                             <div className="p-2 w-1/2">
                                 <div className="relative">
-                                    <label htmlFor="name" className="leading-7 text-sm text-gray-600">
+                                    <label htmlFor="name" className="leading-7 text-sm text-gray-900">
                                         Name
                                     </label>
                                     <input
                                         type="text"
                                         id="name"
                                         name="name"
-                                        className="w-full bg-gray-100 bg-opacity-50 rounded border border-gray-300 focus:border-indigo-500 focus:bg-white focus:ring-2 focus:ring-indigo-200 text-base outline-none text-gray-700 py-1 px-3 leading-8 transition-colors duration-200 ease-in-out"
+                                        className="w-full bg-gray-900 bg-opacity-50 rounded border border-gray-900 focus:border-indigo-500 focus:bg-white focus:ring-2 focus:ring-indigo-200 text-base outline-none text-gray-900 py-1 px-3 leading-8 transition-colors duration-200 ease-in-out"
                                     />
                                 </div>
                             </div>
                             <div className="p-2 w-1/2">
                                 <div className="relative">
-                                    <label htmlFor="email" className="leading-7 text-sm text-gray-600">
+                                    <label htmlFor="email" className="leading-7 text-sm text-gray-900">
                                         Email
                                     </label>
                                     <input
                                         type="email"
                                         id="email"
                                         name="email"
-                                        className="w-full bg-gray-100 bg-opacity-50 rounded border border-gray-300 focus:border-indigo-500 focus:bg-white focus:ring-2 focus:ring-indigo-200 text-base outline-none text-gray-700 py-1 px-3 leading-8 transition-colors duration-200 ease-in-out"
+                                        className="w-full bg-gray-900 bg-opacity-50 rounded border border-gray-900 focus:border-indigo-500 focus:bg-white focus:ring-2 focus:ring-indigo-200 text-base outline-none text-gray-900 py-1 px-3 leading-8 transition-colors duration-200 ease-in-out"
                                     />
                                 </div>
                             </div>
@@ -43,14 +43,14 @@ export default function HelloWorldPrinter() {
                                 <div className="relative">
                                     <label
                                         htmlFor="message"
-                                        className="leading-7 text-sm text-gray-600"
+                                        className="leading-7 text-sm text-gray-900"
                                     >
                                         Message
                                     </label>
                                     <textarea
                                         id="message"
                                         name="message"
-                                        className="w-full bg-gray-100 bg-opacity-50 rounded border border-gray-300 focus:border-indigo-500 focus:bg-white focus:ring-2 focus:ring-indigo-200 h-32 text-base outline-none text-gray-700 py-1 px-3 resize-none leading-6 transition-colors duration-200 ease-in-out"
+                                        className="w-full bg-gray-900 bg-opacity-50 rounded border border-gray-900 focus:border-indigo-500 focus:bg-white focus:ring-2 focus:ring-indigo-200 h-32 text-base outline-none text-gray-900 py-1 px-3 resize-none leading-6 transition-colors duration-200 ease-in-out"
                                         defaultValue={""}
                                     />
                                 </div>
@@ -58,7 +58,7 @@ export default function HelloWorldPrinter() {
                             <div className="p-2 w-full">
                                 <button onClick={()=>{
                                     window.print()
-                                }} className="flex mx-auto text-white bg-indigo-500 border-0 py-2 px-8 focus:outline-none hover:bg-indigo-600 rounded text-lg">
+                                }} className="flex mx-auto text-white bg-indigo-950 border-0 py-2 px-8 focus:outline-none hover:bg-indigo-950 rounded text-lg">
                                     Button
                                 </button>
                             </div>
