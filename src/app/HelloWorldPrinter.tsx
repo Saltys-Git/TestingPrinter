9204b3a371137e530a18caafe58888fde11ab1ea
Saltys-Git <sidurrahmansiam@gmail.com>
2023-07-05 20:43:40 +0600

    Forth commit

--- src/app/HelloWorldPrinter.tsx
+++ src/app/HelloWorldPrinter.tsx
@@ -3,6 +3,8 @@ import useScanDetection from 'use-scan-detection';
 import {useState} from "react";
 export default function HelloWorldPrinter() {
     const [value, setValue] = useState<any>("");
+    const isBrowser = () => typeof window !== 'undefined'; //The approach recommended by Next.js
+
     useScanDetection({
         onComplete: setValue,
         minLength: 13 // EAN13
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,4 +1,4 @@
-"use client"
+'use client'
 import HelloWorldPrinter from "@/app/HelloWorldPrinter";
 
 export default function Home() {
