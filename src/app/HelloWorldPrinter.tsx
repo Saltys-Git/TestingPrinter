bedc8593a8144664effa5707b87191679beca90c
Saltys-Git <sidurrahmansiam@gmail.com>
2023-07-05 21:23:25 +0600

    Forth commit

--- src/app/HelloWorldPrinter.tsx
+++ src/app/HelloWorldPrinter.tsx
@@ -1,19 +1,49 @@
 'use client'
 import useScanDetection from 'use-scan-detection';
-import {useState} from "react";
+import {useEffect, useState} from "react";
+
+
 export default function HelloWorldPrinter() {
-    const [value, setValue] = useState<any>("");
+    const [value, setValue] = useState("asd");
+    const [value2, setValue2] = useState("asd");
     const isBrowser = () => typeof window !== 'undefined'; //The approach recommended by Next.js
+    let barcodeScan = ""
+    useEffect(()=>{
 
-    useScanDetection({
-        onComplete: setValue,
-        minLength: 13 // EAN13
+        function handleKeyDown(e: any) {
+            setValue2(e.key)
+            if (e.key === 13 && barcodeScan.length > 3) {
+                handleScan(barcodeScan)
+                return
+            }
+            if (e.key === 16) {
+                return
+            }
+
+            barcodeScan += e.key
+
+            setTimeout(() => {
+                barcodeScan = ""
+            },100)
+        }
+
+        if (isBrowser()) {
+            document.addEventListener("keydown", handleKeyDown)
+        }
+
+        return function cleanup() {
+            if (isBrowser()) {
+                document.removeEventListener("keydown", handleKeyDown)
+            }
+        }
     })
-
+    const handleScan = (data: string) => {
+        setValue(data);
+    }
     return (
-        <input
-            value={value}
-            type="text"
-        />
+        <div>
+            <p className="text-4xl">{value}</p>
+            <p className="text-4xl">{value2}</p>
+        </div>
     );
 }
